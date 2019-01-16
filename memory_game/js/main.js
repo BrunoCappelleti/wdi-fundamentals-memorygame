505b8412e7a03a9332e33ec0338bac372d5689c2
Bruno <Bruno.Cappelleti@gmail.com>
2019-01-15 23:11:52 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,21 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User has flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
-	}
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+	}	
 }
+var flip = function (cardId) {
+	console.log("User has flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+	checkForMatch();
+}
+
+flip(0);
+flip(2);
